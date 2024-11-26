a7c06f68e80cf51f6b0cef3492f901e397c1a597
snehathomas4427 <snehavechoor@gmail.com>
2024-11-25 23:45:44 -0600

    comments

--- ec_lab.cpp
+++ ec_lab.cpp
@@ -6,30 +6,30 @@ using namespace std;
 double extractNumeric(const string& str){
     bool hasdecimal = false;  // tracks if a decimal point is present
     bool sign = false;     // tracks if a sign is present
-    double value = 0.0;
+    double value = 0.0; // tracks the actual value
     int decimal = 0; // checks for how many numbers are after the decimal
 
     for (int i = 0; i < str.length(); i++){
         if (str[i] >= '0' && str[i] <= '9'){
-            value = (value * 10) + (str[i] - '0');
-            if (hasdecimal == true){
+            value = (value * 10) + (str[i] - '0'); // str[i] - '0' subtracts the ASCII value of the character '0' from str[i]. (turns a char to an int)
+            if (hasdecimal == true){ // if there has been a decimal, increment the amount of nums after the decimal
                 decimal ++;
             }
         }
         else if (str[i] == '.'){
-            if (hasdecimal == true){
+            if (hasdecimal == true){ // if there is already another decimal -> invalid input
                 return -999999.99;
             } else {
                 hasdecimal = true;
             }
         }
         else if (str[i] == '+' || str[i]== '-'){
-            if (sign == true && i != 0){
+            if (sign == true && i != 0){ // if there is another sign (+/-) that's not the first character -> invalid input
                 return -999999.99;
             }else{
                 sign = true;
                 if (str[i] == '-'){
-                    value = -value;
+                    value = value*-1;
                 }
             }
         }
@@ -61,7 +61,6 @@ int main(){
         } else {
             cout << "The input is invalid." << endl;
         }
-        
     }
     return 0;
 }
